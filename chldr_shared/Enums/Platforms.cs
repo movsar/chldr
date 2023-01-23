0e1b6849dab29b6ccfab4fac9a64e858e506e63d
Movsar Bekaev <bekaev.movsar@gmail.com>
2023-01-23 08:17:27 +0300

    added environment service

---
+++ chldr_shared/Enums/Platforms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chldr_shared.Enums
+{
+    public enum Platforms
+    {
+        Web = 0,
+        Windows = 1,
+        Android = 2,
+        IOS = 3,
+    }
+}
---
+++ chldr_shared/Services/EnvironmentService.cs
@@ -0,0 +1,28 @@
+﻿using chldr_shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chldr_shared.Services
+{
+    public static class EnvironmentService
+    {
+        public static Platforms CurrentPlatform
+        {
+            get
+            {
+#if ANDROID
+                return Platforms.Android;
+#elif IOS
+                return Platforms.IOS;
+#elif WINDOWS
+                return Platforms.Windows;
+#else
+                return Platforms.Web;
+#endif
+            }
+        }
+    }
+}
