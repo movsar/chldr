0664034ba0f12269fa6e49da613274815be99e20
Movsar Bekaev <bekaev.movsar@gmail.com>
2023-02-21 17:58:15 +0300

    added translations

--- chldr_data/Enums/WordDetails/VerbMood.cs
+++ chldr_data/Enums/WordDetails/VerbMood.cs
@@ -21,22 +21,19 @@ namespace chldr_data.Enums.WordDetails
 }
 
 /*
- 1. Изъявительное наклонение – БИЛГАЛА САТТАМ
-2. Повелительное наклонение – ТIЕДОЖОРОН САТТАМ
-3. Вопросительная форма изъявительного наклонения – БИЛГАЛА САТТАМАН ХАТТАРАН КЕП
-4. Реально-условное наклонение (обстоятельственная форма глагола) –БАКЪ БОЛУ БЕХКАМАН САТТАМ
-5. Нереально-условное наклонение –БАКЪ БОЦУ БЕХКАМАН САТТАМ
-6. Реально-желательное наклонение – БАКЪ БОЛУ ЛААРАН САТТАМ
-7. Нереально-желательное наклонение – БАКЪ БОЦУ ЛААРАМ САТТАМ 
+    1. Изъявительное наклонение – БИЛГАЛА САТТАМ
+    2. Повелительное наклонение – ТIЕДОЖОРОН САТТАМ
+    3. Вопросительная форма изъявительного наклонения – БИЛГАЛА САТТАМАН ХАТТАРАН КЕП
+    4. Реально-условное наклонение (обстоятельственная форма глагола) –БАКЪ БОЛУ БЕХКАМАН САТТАМ
+    5. Нереально-условное наклонение –БАКЪ БОЦУ БЕХКАМАН САТТАМ
+    6. Реально-желательное наклонение – БАКЪ БОЛУ ЛААРАН САТТАМ
+    7. Нереально-желательное наклонение – БАКЪ БОЦУ ЛААРАМ САТТАМ 
 
-Indicative mood - BILGALA SATTAM (statement)
-Imperative mood - TIYEDOJORON SATTAM (command)
-Interrogative indicative mood - BILGALA SATTAMAN HATTARAN KEP (question/statement)
-Real conditional mood (verbal adverbial form) - BAKY BOLU BEHKAMAN SATTAM (if...then)
-Unreal conditional mood - BAKY BOTSU BEHKAMAN SATTAM (if...then)
-Real desiderative mood - BAKY BOLU LAARAN SATTAM (hope/wish)
-Unreal desiderative mood - BAKY BOTSU LAARAM SATTAM (hope/wish)
-
-
-
+    Indicative mood - BILGALA SATTAM (statement)
+    Imperative mood - TIYEDOJORON SATTAM (command)
+    Interrogative indicative mood - BILGALA SATTAMAN HATTARAN KEP (question/statement)
+    Real conditional mood (verbal adverbial form) - BAKY BOLU BEHKAMAN SATTAM (if...then)
+    Unreal conditional mood - BAKY BOTSU BEHKAMAN SATTAM (if...then)
+    Real desiderative mood - BAKY BOLU LAARAN SATTAM (hope/wish)
+    Unreal desiderative mood - BAKY BOTSU LAARAM SATTAM (hope/wish)
  */
