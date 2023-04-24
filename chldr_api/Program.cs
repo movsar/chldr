890280015209b2e9504b1ac0a02cdd02424b1476
Movsar Bekaev <bekaev.movsar@gmail.com>
2023-04-24 18:30:46 +0300

    added graphql

--- chldr_api/Program.cs
+++ chldr_api/Program.cs
@@ -1,3 +1,5 @@
+using chldr_tools;
+
 namespace chldr_api
 {
     public class Program
@@ -9,26 +11,26 @@ namespace chldr_api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<SqlContext>();
+            builder.Services.AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddProjections()
+                .AddFiltering()
+                .AddSorting();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+             
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
-
             app.MapControllers();
-
+            app.MapGraphQL("/graphql");
             app.Run();
         }
     }
---
+++ chldr_api/Query.cs
@@ -0,0 +1,13 @@
+﻿using chldr_data.Entities;
+using chldr_tools;
+
+namespace chldr_api
+{
+    public class Query
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<SqlEntry> GetEntries([Service] SqlContext context) => context.Entries;
+    }
+}
