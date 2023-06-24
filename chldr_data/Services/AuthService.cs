ca2c1e551d8ce93d9b2b07c85e49d1e22cf172cd
Movsar Bekaev <movsar.dev@gmail.com>
2023-06-24 13:43:38 +0300

    save

--- chldr_data/Services/AuthService.cs
+++ chldr_data/Services/AuthService.cs
@@ -1,6 +1,7 @@
 ﻿using chldr_data.Enums;
 using chldr_data.Models;
 using chldr_data.ResponseTypes;
+using chldr_data.Services;
 using chldr_utils.Interfaces;
 using GraphQL;
 
@@ -8,183 +9,13 @@ namespace chldr_utils.Services
 {
     public class AuthService
     {
-        private readonly IGraphQlClient _requestSender;
+        private readonly RequestService _requestService;
 
-        public AuthService(IGraphQlClient requestSender)
+        public AuthService(RequestService requestService)
         {
-            _requestSender = requestSender;
+            _requestService = requestService;
         }
 
-        public async Task PasswordResetRequestAsync(string email)
-        {
-            var request = new GraphQLRequest
-            {
-                Query = @"
-                mutation($email: String!) {
-                    passwordReset(email: $email) {
-                        success
-                        errorMessage
-                        resetToken
-                    }
-                }",
-                Variables = new { email }
-            };
 
-            var response = await _requestSender.SendRequestAsync<PasswordResetResult>(request, "passwordReset");
-            if (!response.Data.Success)
-            {
-                throw new Exception(response.Data.ErrorMessage);
-            }
-        }
-
-        public async Task UpdatePasswordAsync(string token, string newPassword)
-        {
-            var request = new GraphQLRequest
-            {
-                Query = @"
-                mutation($token: String!, $newPassword: String!) {
-                    updatePassword(token: $token, newPassword: $newPassword) {
-                        success
-                        errorMessage
-                    }
-                }",
-                Variables = new { token, newPassword }
-            };
-
-            var response = await _requestSender.SendRequestAsync<RequestResult>(request, "updatePassword");
-            if (!response.Data.Success)
-            {
-                throw new Exception(response.Data.ErrorMessage);
-            }
-        }
-
-        public async Task ConfirmUserAsync(string token)
-        {
-            var request = new GraphQLRequest
-            {
-                Query = @"
-                        mutation($token: String!) {
-                            confirmEmail(token: $token) {
-                                success
-                                errorMessage
-                            }
-                        }",
-
-                Variables = new { token }
-            };
-
-            var response = await _requestSender.SendRequestAsync<RequestResult>(request, "confirmEmail");
-            if (!response.Data.Success)
-            {
-                throw new Exception(response.Data.ErrorMessage);
-            }
-        }
-
-        internal async Task<ActiveSession> LogInEmailPasswordAsync(string email, string password)
-        {
-            var request = new GraphQLRequest
-            {
-                Query = @"
-                        mutation($email: String!, $password: String!) {
-                            loginEmailPassword(email: $email, password: $password) {
-                                success
-                                errorMessage
-                                accessToken
-                                refreshToken
-                                accessTokenExpiresIn
-                                user {
-                                    userId,
-                                    email,
-                                    firstName,
-                                    lastName,
-                                    rate
-                                }
-                            }
-                        }",
-
-                Variables = new { email, password }
-            };
-
-            var response = await _requestSender.SendRequestAsync<LoginResult>(request, "loginEmailPassword");
-            if (!response.Data.Success)
-            {
-                throw new Exception(response.Data.ErrorMessage);
-            }
-
-            return new ActiveSession()
-            {
-                AccessToken = response.Data.AccessToken,
-                RefreshToken = response.Data.RefreshToken,
-                AccessTokenExpiresIn = (DateTimeOffset)response.Data.AccessTokenExpiresIn,
-                Status = SessionStatus.LoggedIn,
-                User = response.Data.User
-            };
-        }
-
-        internal async Task<string> RegisterUserAsync(string email, string password)
-        {
-            var request = new GraphQLRequest
-            {
-                Query = @"
-                        mutation($email: String!, $password: String!) {
-                            registerUser(email: $email, password: $password) {
-                                success
-                                errorMessage
-                                token
-                            }
-                        }",
-
-                Variables = new { email, password }
-            };
-
-            var response = await _requestSender.SendRequestAsync<RegistrationResult>(request, "registerUser");
-            if (!response.Data.Success)
-            {
-                throw new Exception(response.Data.ErrorMessage);
-            }
-
-            if (string.IsNullOrWhiteSpace(response.Data.Token))
-            {
-                throw new Exception("Token shouldn't be null");
-            }
-
-            return response.Data.Token;
-        }
-
-        internal async Task<ActiveSession> RefreshTokens(string refreshToken)
-        {
-            var request = new GraphQLRequest
-            {
-                Query = @"
-                        mutation($refreshToken: String!) {
-                            logInRefreshToken(refreshToken: $refreshToken) {
-                                success
-                                errorMessage
-                                accessToken
-                                refreshToken
-                                accessTokenExpiresIn
-                                user {
-                                    userId,
-                                    email,
-                                    firstName,
-                                    lastName,
-                                    rate
-                                }
-                            }
-                        }",
-
-                Variables = new { refreshToken }
-            };
-
-            var response = await _requestSender.SendRequestAsync<LoginResult>(request, "logInRefreshToken");
-            return new ActiveSession()
-            {
-                AccessToken = response.Data.Success ? response.Data.AccessToken! : "",
-                RefreshToken = response.Data.Success ? response.Data.RefreshToken! : "",
-                AccessTokenExpiresIn = response.Data.Success ? (DateTimeOffset)response.Data.AccessTokenExpiresIn! : DateTime.UtcNow,
-                Status = response.Data.Success ? SessionStatus.LoggedIn : SessionStatus.Anonymous,
-                User = response.Data.Success ? response.Data.User : null
-            };
-        }
     }
 }
--- chldr_data/Services/RequestService.cs
+++ chldr_data/Services/RequestService.cs
@@ -1,5 +1,6 @@
 ﻿using chldr_data.DatabaseObjects.Dtos;
 using chldr_data.Enums;
+using chldr_data.Models;
 using chldr_data.ResponseTypes;
 using chldr_utils.Interfaces;
 using GraphQL;
@@ -14,7 +15,176 @@ namespace chldr_data.Services
             _graphQLRequestSender = graphQLRequestSender;
         }
         private IGraphQlClient _graphQLRequestSender;
+        public async Task UpdatePasswordAsync(string token, string newPassword)
+        {
+            var request = new GraphQLRequest
+            {
+                Query = @"
+                mutation($token: String!, $newPassword: String!) {
+                    updatePassword(token: $token, newPassword: $newPassword) {
+                        success
+                        errorMessage
+                    }
+                }",
+                Variables = new { token, newPassword }
+            };
 
+            var response = await _graphQLRequestSender.SendRequestAsync<RequestResult>(request, "updatePassword");
+            if (!response.Data.Success)
+            {
+                throw new Exception(response.Data.ErrorMessage);
+            }
+        }
+
+        public async Task ConfirmUserAsync(string token)
+        {
+            var request = new GraphQLRequest
+            {
+                Query = @"
+                        mutation($token: String!) {
+                            confirmEmail(token: $token) {
+                                success
+                                errorMessage
+                            }
+                        }",
+
+                Variables = new { token }
+            };
+
+            var response = await _graphQLRequestSender.SendRequestAsync<RequestResult>(request, "confirmEmail");
+            if (!response.Data.Success)
+            {
+                throw new Exception(response.Data.ErrorMessage);
+            }
+        }
+
+        internal async Task<ActiveSession> LogInEmailPasswordAsync(string email, string password)
+        {
+            var request = new GraphQLRequest
+            {
+                Query = @"
+                        mutation($email: String!, $password: String!) {
+                            loginEmailPassword(email: $email, password: $password) {
+                                success
+                                errorMessage
+                                accessToken
+                                refreshToken
+                                accessTokenExpiresIn
+                                user {
+                                    userId,
+                                    email,
+                                    firstName,
+                                    lastName,
+                                    rate
+                                }
+                            }
+                        }",
+
+                Variables = new { email, password }
+            };
+
+            var response = await _graphQLRequestSender.SendRequestAsync<LoginResult>(request, "loginEmailPassword");
+            if (!response.Data.Success)
+            {
+                throw new Exception(response.Data.ErrorMessage);
+            }
+
+            return new ActiveSession()
+            {
+                AccessToken = response.Data.AccessToken,
+                RefreshToken = response.Data.RefreshToken,
+                AccessTokenExpiresIn = (DateTimeOffset)response.Data.AccessTokenExpiresIn,
+                Status = SessionStatus.LoggedIn,
+                User = response.Data.User
+            };
+        }
+
+        internal async Task<string> RegisterUserAsync(string email, string password)
+        {
+            var request = new GraphQLRequest
+            {
+                Query = @"
+                        mutation($email: String!, $password: String!) {
+                            registerUser(email: $email, password: $password) {
+                                success
+                                errorMessage
+                                token
+                            }
+                        }",
+
+                Variables = new { email, password }
+            };
+
+            var response = await _graphQLRequestSender.SendRequestAsync<RegistrationResult>(request, "registerUser");
+            if (!response.Data.Success)
+            {
+                throw new Exception(response.Data.ErrorMessage);
+            }
+
+            if (string.IsNullOrWhiteSpace(response.Data.Token))
+            {
+                throw new Exception("Token shouldn't be null");
+            }
+
+            return response.Data.Token;
+        }
+
+        internal async Task<ActiveSession> RefreshTokens(string refreshToken)
+        {
+            var request = new GraphQLRequest
+            {
+                Query = @"
+                        mutation($refreshToken: String!) {
+                            logInRefreshToken(refreshToken: $refreshToken) {
+                                success
+                                errorMessage
+                                accessToken
+                                refreshToken
+                                accessTokenExpiresIn
+                                user {
+                                    userId,
+                                    email,
+                                    firstName,
+                                    lastName,
+                                    rate
+                                }
+                            }
+                        }",
+
+                Variables = new { refreshToken }
+            };
+
+            var response = await _graphQLRequestSender.SendRequestAsync<LoginResult>(request, "logInRefreshToken");
+            return new ActiveSession()
+            {
+                AccessToken = response.Data.Success ? response.Data.AccessToken! : "",
+                RefreshToken = response.Data.Success ? response.Data.RefreshToken! : "",
+                AccessTokenExpiresIn = response.Data.Success ? (DateTimeOffset)response.Data.AccessTokenExpiresIn! : DateTime.UtcNow,
+                Status = response.Data.Success ? SessionStatus.LoggedIn : SessionStatus.Anonymous,
+                User = response.Data.Success ? response.Data.User : null
+            };
+        }
+        public async Task PasswordResetRequestAsync(string email)
+        {
+            var request = new GraphQLRequest
+            {
+                Query = @"
+                mutation($email: String!) {
+                    passwordReset(email: $email) {
+                        success
+                        errorMessage
+                        resetToken
+                    }
+                }",
+                Variables = new { email }
+            };
+
+            var response = await _graphQLRequestSender.SendRequestAsync<PasswordResetResult>(request, "passwordReset");
+            if (!response.Data.Success)
+            {
+                throw new Exception(response.Data.ErrorMessage);
+            }
+        }
         public async Task<IEnumerable<T>> TakeLast<T>(RecordType recordType, int count)
         {
             var operation = "takeLast";
