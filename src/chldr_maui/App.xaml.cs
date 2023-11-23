a552cba8c7d05355d57445104a783667899ec8ae
= <movsar.dev@gmail.com>
2023-11-23 18:49:48 +0300

    upgraded the projects to .net 8

---
+++ src/chldr_maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace dosham
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/chldr_maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace dosham
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
