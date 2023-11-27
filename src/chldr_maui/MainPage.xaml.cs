99e9879846fac33b59e2c0b9f6df52146e24006c
= <movsar.dev@gmail.com>
2023-11-27 06:28:15 +0300

    added ReactiveUI

--- src/chldr_maui/MainPage.xaml.cs
+++ src/chldr_maui/MainPage.xaml.cs
@@ -1,34 +1,53 @@
 ﻿using dosham.Stores;
+using dosham.ViewModels;
+using ReactiveUI;
+using ReactiveUI.Maui;
+using System.Reactive.Disposables;
 
 namespace dosham
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, IViewFor<MainPageViewModel>
     {
-        private  ContentStore _contentStore;
+        public static readonly BindableProperty ViewModelProperty = BindableProperty.Create(
+            nameof(ViewModel), typeof(MainPageViewModel), typeof(MainPage), null, BindingMode.OneWay);
+
+        public MainPageViewModel ViewModel
+        {
+            get => (MainPageViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (MainPageViewModel)value;
+        }
+        private ContentStore _contentStore;
         int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            ViewModel = new MainPageViewModel();
+
+            this.WhenActivated(disposables =>
+            {
+                this.BindCommand(ViewModel, vm => vm.CounterCommand, v => v.CounterBtn)
+                    .DisposeWith(disposables);
+
+                this.OneWayBind(ViewModel, vm => vm.Count, v => v.CounterBtn.Text,
+                                count => $"Clicked {count} " + (count == 1 ? "time" : "times"))
+                    .DisposeWith(disposables);
+            });
         }
 
         protected override void OnHandlerChanged()
         {
             base.OnHandlerChanged();
-            _contentStore = this.Handler.MauiContext.Services.GetRequiredService<ContentStore>();
+            _contentStore = Handler.MauiContext.Services.GetRequiredService<ContentStore>();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 
 }
---
+++ src/chldr_maui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveUI;
+using System.Reactive;
+
+namespace dosham.ViewModels
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        private int _count;
+        public int Count
+        {
+            get => _count;
+            set => this.RaiseAndSetIfChanged(ref _count, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> CounterCommand { get; }
+
+        public MainPageViewModel()
+        {
+            CounterCommand = ReactiveCommand.Create(IncrementCount);
+        }
+
+        private void IncrementCount()
+        {
+            Count++;
+        }
+    }
+}
