0f6b4c8a7472b0c59a4ffb0f75dc9d1d732aaa97
Movsar Bekaev <movsar.dev@gmail.com>
2024-01-13 17:05:41 +0300

    added email confirmation controller

---
+++ src/chldr_api/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using chldr_api.GraphQL.MutationServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chldr_api.Controllers
+{
+    [Route("user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserResolver _userResolver;
+        private readonly IHttpContextAccessor _httpContext;
+        public UserController(UserResolver userResolver, IHttpContextAccessor httpContext)
+        {
+            _userResolver = userResolver;
+            _httpContext = httpContext;
+        }
+
+        [HttpGet("confirm")]
+        public async Task<IActionResult> ConfirmEmail(string token)
+        {
+            try
+            {
+                await _userResolver.ConfirmEmailAsync(token);
+                return Ok("Ваша учетная запись успешно активирована! Это можно закрыть.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
