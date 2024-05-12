68f5194423b433f07aab862aefa669489063d4d7
Movsar Bekaev <movsar.dev@gmail.com>
2024-05-12 10:56:35 +0300

    save

--- src/chldr_blazor/Program.cs
+++ src/chldr_blazor/Program.cs
@@ -1,5 +1,10 @@
+using Blazored.Modal;
+using chldr_app.Services;
+using core.Enums;
+using core.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
 
 namespace chldr_blazor
 {
@@ -13,12 +18,15 @@ namespace chldr_blazor
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddOidcAuthentication(options =>
-            {
-                // Configure your authentication provider options here.
-                // For more information, see https://aka.ms/blazor-standalone-auth
-                builder.Configuration.Bind("Local", options.ProviderOptions);
-            });
+            // Add services to the container.          
+            ServiceRegistrator.RegisterCommonServices(builder.Services);
+
+            builder.Services.AddBlazoredModal();
+
+            builder.Services.AddTransient(x => new EnvironmentService(Platforms.Web, true));
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("ru-RU");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("ru-RU");
 
             await builder.Build().RunAsync();
         }
