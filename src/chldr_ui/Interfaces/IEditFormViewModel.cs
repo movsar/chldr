a02222def4c02801cd7c398168539c3d21f10236
Movsar Bekaev <movsar.dev@gmail.com>
2024-09-29 18:19:56 +0300

    sv

--- src/chldr_ui/Interfaces/IEditFormViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace chldr_ui.Interfaces
-{
-    public interface IEditFormViewModel<TFormDto, TFormValidator>
-    {
-        bool FormSubmitted { get; set; }
-        Task ValidateAndSubmitAsync(TFormDto formDto, Func<Task> function, string[]? validationRuleSets = null);
-    }
-}
